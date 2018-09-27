06f6fef1f415fc86eed7b34e6f832353add32380
SONG Jianzhong <jsong12@slb.com>
2018-09-27 14:17:20 +0800

    add print-object-prototype

---
+++ snippets/js-object/print-object-prototype.js
@@ -0,0 +1,11 @@
+document.printPrototypeChain = object => {
+  var proto = object.constructor.prototype;
+  var result = '';
+
+  while (proto) {
+    result += ' -> ' + proto.constructor.name;
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return result;
+};
