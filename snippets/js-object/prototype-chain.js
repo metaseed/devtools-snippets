b9cfc6a3a28501396b0e52a7bc10946c97fe3265
SONG Jianzhong <jsong12@slb.com>
2018-09-27 15:12:22 +0800

    add findOnPrototypeChain function

---
+++ snippets/js-object/prototype-chain.js
@@ -0,0 +1,24 @@
+printPrototypeChain = object => {
+  var proto = object.__proto__ || object.constructor.prototype;
+  var result = '';
+
+  while (proto) {
+    result += ' -> ' + proto.constructor.name;
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return result.slice(4);
+};
+
+findOnPrototypeChain = (object, property) => {
+  var proto = object.__proto__ || object.constructor.prototype;
+  var result = object.hasOwnProperty(property) ? `(${property})` : '';
+
+  while (proto) {
+    result += ' -> ' + proto.constructor.name;
+    result += proto.hasOwnProperty(property) ? `(${property})` : '';
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return result;
+};
